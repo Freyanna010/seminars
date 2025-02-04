02606b596a2715795336a6379b9193542512baac
Ann <socevilaf@gmail.com>
2025-02-04 13:18:44 +0300

    Add SeminarsStore  and fetch seminarsData

--- src/component/App/App.tsx
+++ src/component/App/App.tsx
@@ -1,12 +1,25 @@
-import { FC } from "react";
+import seminarsStore from "@/store/seminarsStore";
+import { toJS } from "mobx";
+import { observer } from "mobx-react-lite";
+import { FC, useEffect } from "react";
 
 
 const App: FC = () => {
+ 
+  useEffect ( () => {
+    seminarsStore.fetchSeminarsData()
+  } ,[])
+
+  useEffect ( () => {
+   console.log("seminar", toJS(seminarsStore.seminarsData) )
+  } ,[seminarsStore.seminarsData])
+
+
   return (
-    <>
-seminars
-    </>
+    <div>
+{seminarsStore.seminarsData.map(seminar => seminar.id)}
+    </div>
   );
 };
 
-export default App;
+export default observer(App);
---
+++ src/store/seminarsStore.ts
@@ -0,0 +1,40 @@
+import { SeminarsData } from "@/types/types";
+import axios from "axios";
+import { action, makeObservable, observable, runInAction } from "mobx";
+
+class SeminarsStore {
+  isLoading = false;
+  errorMessage: string | null = null;
+  seminarsData: SeminarsData = [];
+
+  constructor() {
+    makeObservable(this, {
+      isLoading: observable,
+      errorMessage: observable,
+      seminarsData: observable,
+      fetchSeminarsData: action,
+    });
+  }
+
+  fetchSeminarsData = async () => {
+    this.isLoading = true;
+    this.errorMessage = null;
+
+    try {
+      const response = await axios.get("/data/seminars.json");
+      runInAction(() => {
+        this.seminarsData = response.data.seminars;
+      });
+    } catch (error) {
+      runInAction(() => {
+        this.errorMessage = "Ошибка загрузки данных";
+      });
+    } finally {
+      runInAction(() => {
+        this.isLoading = false;
+      });
+    }
+  };
+}
+
+export default new SeminarsStore();
