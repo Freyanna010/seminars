9c024a021c5ae419f24299c66797780828692f22
Ann <socevilaf@gmail.com>
2025-02-04 10:23:47 +0300

    Initial commit

---
+++ src/component/App/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+
+const App: FC = () => {
+  return (
+    <>
+seminars
+    </>
+  );
+};
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import App from "./component/App/App.tsx";
+
+
+createRoot(document.getElementById("root")!).render( 
+    <App />
+);
